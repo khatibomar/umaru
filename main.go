ff3ee05e8d2c0eecbccc897ca75290a192f4251a
omarkhatib <elkhatibomar@outlook.com>
2023-01-21 17:31:50 +0200

    Add basic template and routes for pages

--- main.go
+++ main.go
@@ -12,8 +12,42 @@ type ViewData struct {
 	Year string
 }
 
-func home(w http.ResponseWriter, req *http.Request) {
-	ts, err := template.ParseFiles("./templates/home.html")
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	renderPage(w, "home")
+}
+
+func readings(w http.ResponseWriter, r *http.Request) {
+	renderPage(w, "readings")
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	renderPage(w, "about")
+}
+
+func quotes(w http.ResponseWriter, r *http.Request) {
+	renderPage(w, "quotes")
+}
+
+func snippets(w http.ResponseWriter, r *http.Request) {
+	renderPage(w, "snippets")
+}
+
+func talks(w http.ResponseWriter, r *http.Request) {
+	renderPage(w, "talks")
+}
+
+func renderPage(w http.ResponseWriter, page string) {
+	files := []string{
+		"./templates/base.html",
+		"./templates/" + page + ".html",
+	}
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", 500)
@@ -23,7 +57,7 @@ func home(w http.ResponseWriter, req *http.Request) {
 	year := ViewData{
 		Year: strconv.Itoa(time.Now().Year()),
 	}
-	err = ts.Execute(w, year)
+	err = ts.ExecuteTemplate(w, "base", year)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", 500)
@@ -33,18 +67,16 @@ func home(w http.ResponseWriter, req *http.Request) {
 func main() {
 	mux := http.NewServeMux()
 
-	// Create a file server which serves files out of the "./ui/static" directory.
-	// Note that the path given to the http.Dir function is relative to the project
-	// directory root.
 	fileServer := http.FileServer(http.Dir("./assets"))
 
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	// Register the other application routes as normal.
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/readings", readings)
+	mux.HandleFunc("/talks", talks)
+	mux.HandleFunc("/quotes", quotes)
+	mux.HandleFunc("/snippets", snippets)
+	mux.HandleFunc("/about", about)
 
 	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
